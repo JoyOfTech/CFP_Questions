03fae42a41a65abc43f49ab51f53b9993fbb9b57
JoyOfTech <joy.m.whitt@gmail.com>
2015-03-10 23:12:55 -0400

    Added Comments to Admin

--- app/questions/admin.py
+++ app/questions/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from questions.models import Question
+from questions.models import Question, Comment
 # Register your models here.
 
-admin.site.register(Question)
+
+class CommentInline(admin.StackedInline): #I need to figure out why this works
+    model = Comment
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+    
+    ]
+    inlines = [CommentInline]
+
+admin.site.register(Question, QuestionAdmin) #per polls app tutorial
 #Custom code makes this viewable on the admin page (runserver)
+
+
+
+
