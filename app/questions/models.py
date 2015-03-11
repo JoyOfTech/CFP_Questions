60f349ff510b98dba5048568d2071264a1714c11
JoyOfTech <joy.m.whitt@gmail.com>
2015-03-11 19:55:01 -0400

    updated views.py, created template

--- app/questions/models.py
+++ app/questions/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 
-# Create your models here.
 
 class Question(models.Model):
     #models = tables in database. we want fields for questions and fields
     #for comment. Think of this as Excel doc. These are headers/columns
-    #hypothetical Excel spreadsheet called "Question". Column in "Question sheet called
-    #question_text
+    #hypothetical Excel spreadsheet called "Question". Column in "Question sheet
+    #called question_text
     question_text = models.CharField(max_length=200)
 
 
 class Comment(models.Model):
     comment_text = models.CharField(max_length=200)
 
-    #I also need you to be able to go back and find a question for me
-    #don't ask the question which comment it's related to
-    #ask the comment what question
+    #go back and find a question.
+    #Don't ask the question which comment it's related to
+    #ask the comment to what question it belongs
     question = models.ForeignKey(Question)
     #'question' is a field in "Comment"
--- app/questions/views.py
+++ app/questions/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+#from django.http import HttpResponse <-- I don't need this because of "render"
 
-# Create your views here.
+from django.shortcuts import render
+#render(request, template_name[, dictionary][, context_instance][, content_type][, status][, current_app][, dirs])[source]
+#Combines a given template with a given context dictionary and
+#returns an HttpResponse object with that rendered text.
+
+from questions.models import Question
+
+#the code below is from the index/detail portion of the polls app
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
+
+#the code below is from https://github.com/franciskung/leadnow/blob/master/reports/views.py
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'questions/index.html', context)
+
+from django.contrib import messages #will this help with the "comments" page?
+from questions.models import Comment
+
+def home(request):
+    return render(request, 'home.html', {})
+
+# Display a list of saved queries, plus an area to enter a new  query  
+def query(request):
+    comments  = Comment.objects.all()
+    queries = Query.objects.all()
+  
+    return render(request, 'query.html', {'queries': queries,
+                                         'comments': comments,
+                                          
+                                        })
+
+# When we have saved queries, this will let you see the details of a saved query
+def query_details(request, query_id):
+    return render(request, 'query.html', {})
+
