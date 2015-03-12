78fbfcc975aecef35bb0a84bbaa486e9011d5f2e
JoyOfTech <joy.m.whitt@gmail.com>
2015-03-12 13:18:53 -0400

    added templates

--- app/questions/models.py
+++ app/questions/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 
 class Question(models.Model):
+    question_text = models.CharField(max_length=200)
     #models = tables in database. we want fields for questions and fields
     #for comment. Think of this as Excel doc. These are headers/columns
-    #hypothetical Excel spreadsheet called "Question". Column in "Question sheet
+    #hypothetical Excel spreadsheet called "Question". Column in "Question" sheet
     #called question_text
-    question_text = models.CharField(max_length=200)
 
 
 class Comment(models.Model):
     comment_text = models.CharField(max_length=200)
-
+    question = models.ForeignKey(Question) #many-to-one relationship
+    
     #go back and find a question.
     #Don't ask the question which comment it's related to
     #ask the comment to what question it belongs
-    question = models.ForeignKey(Question)
     #'question' is a field in "Comment"
