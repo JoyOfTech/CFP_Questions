b9d3904b81fb957c3bca20d84c21f8fd8e7ed4e5
Joy Whitt <joy.m.whitt@gmail.com>
2015-03-13 10:34:24 -0400

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'questions',
+    'questions', #specific to this project
 )
 
 MIDDLEWARE_CLASSES = (
